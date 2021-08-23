51b1879e34199fcba0ac708cfbedd2c7b33841ae
m canals <mk1961@gmx.com>
2021-08-23 23:16:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+
+namespace XLIFextract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("XLIFextract (c) 2021 miguel canals MIT License");
+            bool commandlineerrors = false;
+            string inpfileORI = "none";
+            string segtype = "";
+            if (args.Length == 1)
+            {
+                inpfileORI = args[0].Trim();
+                if (!File.Exists(inpfileORI))
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("ERROR -> Looks like XLIF file '{0}' does not exist", inpfileORI);
+                    commandlineerrors = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("ERROR -> Specify only the xlif file");
+                commandlineerrors = true;
+            }
+            // continue
+
+            if (commandlineerrors)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("This program reads and xlif that follow opentm2 export format and exports to an xlsx file. The correct format is: ");
+                Console.WriteLine("");
+                Console.WriteLine("XLIFextract.exe path\\file.xlf");
+                Console.WriteLine("");
+                return;
+            }
+
+            Console.WriteLine("File   -> {0}", inpfileORI);
+
+            XmlSerializer ser = new XmlSerializer(typeof(xliff));
+            xliff myxliff;
+            using (Stream stream = new FileStream(inpfileORI, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                myxliff = (xliff)ser.Deserialize(stream);
+            }
+
+            if (myxliff!=null)
+            {
+                Console.WriteLine("File {0} is valid", inpfileORI);
+            } else
+            {
+                Console.WriteLine("ERROR -> File {0} is NOT valid.", inpfileORI);
+                Console.WriteLine("");
+                Console.WriteLine("Fatal error. ");
+                Console.WriteLine("File should follow the xliff standard for opentm2 folders. ");
+                return;
+
+            } 
+
+            var totalsegments = new List<xliffFileUnitSegment>();
+            var totalids = new List<string>();
+            XLIFextract.xliffFile[] files = myxliff.file;
+            foreach (var file in files)
+            {
+                xliffFileUnit[] units = file.unit;
+                foreach (var unit in units)
+                {
+                    string unitid = unit.id;
+                    xliffFileUnitSegment[] segments = unit.segment;
+                    foreach (var segment in segments)
+                    {
+                        /* Console.WriteLine(segment.state);
+                        Console.WriteLine(segment.wordcount);
+                        Console.WriteLine(segment.source);
+                        Console.WriteLine(segment.target); */
+                        totalsegments.Add(segment);
+                        totalids.Add(unitid);
+                    }
+                        
+                }
+
+            }
+
+
+            // got all segments in total
+
+            int RC = CreateExcel(totalsegments,totalids, inpfileORI);
+
+            
+
+            Console.WriteLine("EOP.");
+
+            int CreateExcel(List<xliffFileUnitSegment> segmentlist, List<string> segids, string xlffile)
+            {
+                string exten = Path.GetExtension(xlffile);
+                string sheetname = Path.GetFileNameWithoutExtension(xlffile);
+                string xlsxfile = xlffile.Replace(exten, ".xlsx");
+
+                using (var workbook = new XLWorkbook())
+                {
+                    var worksheet = workbook.Worksheets.Add(sheetname);
+                    int filenum = 1;
+                    worksheet.Cell("A1").Value = "state";
+                    worksheet.Cell("B1").Value = "wct";
+                    worksheet.Cell("C1").Value = "source";
+                    worksheet.Cell("D1").Value = "target";
+                    worksheet.Column("C").Width = 100;
+                    worksheet.Column("C").Style.Alignment.WrapText=true;
+                    worksheet.Column("D").Width = 100;
+                    worksheet.Column("D").Style.Alignment.WrapText = true;
+                    foreach (var seg in segmentlist)
+                    {
+                        filenum += 1;
+                        worksheet.Cell(string.Format("A{0}", filenum)).Value = seg.state;
+                        worksheet.Cell(string.Format("B{0}", filenum)).Value = seg.wordcount;                        
+                        worksheet.Cell(string.Format("C{0}", filenum)).Value = seg.source;
+                        worksheet.Cell(string.Format("D{0}", filenum)).Value = seg.target;
+                    }
+
+                    filenum = 1;
+                    worksheet.Cell("E1").Value = "infoid";
+                    worksheet.Column("E").Width = 100;
+                    foreach (var segid  in segids)
+                    {
+                        filenum += 1;
+                        worksheet.Cell(string.Format("E{0}", filenum)).Value = segid;                        
+                    }
+
+                    // worksheet.Cell("A1").Value = "Hello World!";
+                    //# worksheet.Cell("A2").FormulaA1 = "=MID(A1, 7, 5)";
+                    workbook.SaveAs(xlsxfile );
+                }
+
+                return 0;
+
+
+            }
+
+
+        }
+
+    }
+}
---
+++ XLIF.cs
@@ -0,0 +1,665 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XLIFextract
+{
+    // 
+    // Este código fuente fue generado automáticamente por xsd, Versión=4.8.3928.0.
+    // 
+
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "urn:oasis:names:tc:xliff:document:2.0", IsNullable = false)]
+    public partial class xliff
+    {
+
+        private xliffFile[] fileField;
+
+        private xliffWordcounts[] wordcountsField;
+
+        private string versionField;
+
+        private string srcLangField;
+
+        private string trgLangField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("file")]
+        public xliffFile[] file
+        {
+            get
+            {
+                return this.fileField;
+            }
+            set
+            {
+                this.fileField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("wordcounts")]
+        public xliffWordcounts[] wordcounts
+        {
+            get
+            {
+                return this.wordcountsField;
+            }
+            set
+            {
+                this.wordcountsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string version
+        {
+            get
+            {
+                return this.versionField;
+            }
+            set
+            {
+                this.versionField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string srcLang
+        {
+            get
+            {
+                return this.srcLangField;
+            }
+            set
+            {
+                this.srcLangField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string trgLang
+        {
+            get
+            {
+                return this.trgLangField;
+            }
+            set
+            {
+                this.trgLangField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffFile
+    {
+
+        private xliffFileNotes[] notesField;
+
+        private xliffFileUnit[] unitField;
+
+        private string idField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("notes")]
+        public xliffFileNotes[] notes
+        {
+            get
+            {
+                return this.notesField;
+            }
+            set
+            {
+                this.notesField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("unit")]
+        public xliffFileUnit[] unit
+        {
+            get
+            {
+                return this.unitField;
+            }
+            set
+            {
+                this.unitField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffFileNotes
+    {
+
+        private string noteField;
+
+        /// <remarks/>
+        public string note
+        {
+            get
+            {
+                return this.noteField;
+            }
+            set
+            {
+                this.noteField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffFileUnit
+    {
+
+        private xliffFileUnitSegment[] segmentField;
+
+        private string idField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("segment")]
+        public xliffFileUnitSegment[] segment
+        {
+            get
+            {
+                return this.segmentField;
+            }
+            set
+            {
+                this.segmentField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffFileUnitSegment
+    {
+
+        private string sourceField;
+
+        private string targetField;
+
+        private string stateField;
+
+        private string segnumberField;
+
+        private string markupField;
+
+        private string wordcountField;
+
+        private string previousField;
+
+        /// <remarks/>
+        public string source
+        {
+            get
+            {
+                return this.sourceField;
+            }
+            set
+            {
+                this.sourceField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string target
+        {
+            get
+            {
+                return this.targetField;
+            }
+            set
+            {
+                this.targetField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string state
+        {
+            get
+            {
+                return this.stateField;
+            }
+            set
+            {
+                this.stateField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string segnumber
+        {
+            get
+            {
+                return this.segnumberField;
+            }
+            set
+            {
+                this.segnumberField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string markup
+        {
+            get
+            {
+                return this.markupField;
+            }
+            set
+            {
+                this.markupField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string wordcount
+        {
+            get
+            {
+                return this.wordcountField;
+            }
+            set
+            {
+                this.wordcountField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string previous
+        {
+            get
+            {
+                return this.previousField;
+            }
+            set
+            {
+                this.previousField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffWordcounts
+    {
+
+        private xliffWordcountsFinal[] finalField;
+
+        private xliffWordcountsExact[] exactField;
+
+        private xliffWordcountsMachine[] machineField;
+
+        private xliffWordcountsNew[] newField;
+
+        private xliffWordcountsDuplicate[] duplicateField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("final")]
+        public xliffWordcountsFinal[] final
+        {
+            get
+            {
+                return this.finalField;
+            }
+            set
+            {
+                this.finalField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("exact")]
+        public xliffWordcountsExact[] exact
+        {
+            get
+            {
+                return this.exactField;
+            }
+            set
+            {
+                this.exactField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("machine")]
+        public xliffWordcountsMachine[] machine
+        {
+            get
+            {
+                return this.machineField;
+            }
+            set
+            {
+                this.machineField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("new")]
+        public xliffWordcountsNew[] @new
+        {
+            get
+            {
+                return this.newField;
+            }
+            set
+            {
+                this.newField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("duplicate")]
+        public xliffWordcountsDuplicate[] duplicate
+        {
+            get
+            {
+                return this.duplicateField;
+            }
+            set
+            {
+                this.duplicateField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffWordcountsFinal
+    {
+
+        private string wcField;
+
+        private string eventsField;
+
+        /// <remarks/>
+        public string wc
+        {
+            get
+            {
+                return this.wcField;
+            }
+            set
+            {
+                this.wcField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string events
+        {
+            get
+            {
+                return this.eventsField;
+            }
+            set
+            {
+                this.eventsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffWordcountsExact
+    {
+
+        private string wcField;
+
+        private string eventsField;
+
+        /// <remarks/>
+        public string wc
+        {
+            get
+            {
+                return this.wcField;
+            }
+            set
+            {
+                this.wcField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string events
+        {
+            get
+            {
+                return this.eventsField;
+            }
+            set
+            {
+                this.eventsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffWordcountsMachine
+    {
+
+        private string wcField;
+
+        private string eventsField;
+
+        /// <remarks/>
+        public string wc
+        {
+            get
+            {
+                return this.wcField;
+            }
+            set
+            {
+                this.wcField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string events
+        {
+            get
+            {
+                return this.eventsField;
+            }
+            set
+            {
+                this.eventsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffWordcountsNew
+    {
+
+        private string wcField;
+
+        private string eventsField;
+
+        /// <remarks/>
+        public string wc
+        {
+            get
+            {
+                return this.wcField;
+            }
+            set
+            {
+                this.wcField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string events
+        {
+            get
+            {
+                return this.eventsField;
+            }
+            set
+            {
+                this.eventsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    public partial class xliffWordcountsDuplicate
+    {
+
+        private string wcField;
+
+        private string eventsField;
+
+        /// <remarks/>
+        public string wc
+        {
+            get
+            {
+                return this.wcField;
+            }
+            set
+            {
+                this.wcField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string events
+        {
+            get
+            {
+                return this.eventsField;
+            }
+            set
+            {
+                this.eventsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "urn:oasis:names:tc:xliff:document:2.0")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "urn:oasis:names:tc:xliff:document:2.0", IsNullable = false)]
+    public partial class NewDataSet
+    {
+
+        private xliff[] itemsField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("xliff")]
+        public xliff[] Items
+        {
+            get
+            {
+                return this.itemsField;
+            }
+            set
+            {
+                this.itemsField = value;
+            }
+        }
+    }
+
+}
